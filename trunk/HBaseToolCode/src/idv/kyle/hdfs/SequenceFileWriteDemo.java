e93f6f338a071b7aa0ee55eab8f7ad0461f457bc
kylelin2000 <kylelin2000@fbfa5a9a-6df7-3eb1-ade7-53795e5f5e70>
2014-01-07 08:22:27 +0000

    [Hadoop Tool]save sequence file

--- trunk/HBaseToolCode/src/idv/kyle/hdfs/SequenceFileWriteDemo.java
+++ trunk/HBaseToolCode/src/idv/kyle/hdfs/SequenceFileWriteDemo.java
@@ -2,7 +2,14 @@ package idv.kyle.hdfs;
 
 import java.io.IOException;
 import java.net.URI;
-import java.security.PrivilegedExceptionAction;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -11,7 +18,6 @@ import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.security.UserGroupInformation;
 
 public class SequenceFileWriteDemo {
 
@@ -20,38 +26,65 @@ public class SequenceFileWriteDemo {
       "Seven, eight, lay them straight", "Nine, ten, a big fat hen" };
 
   public static void main(String[] args) throws IOException,
-      InterruptedException {
-    UserGroupInformation ugi = UserGroupInformation.createRemoteUser("kyle");
+      InterruptedException, LoginException {
+    System.setProperty("sun.security.krb5.debug", "true");
+    System.setProperty("java.security.krb5.realm", "LOCALDOMAIN");
+    System.setProperty("java.security.krb5.kdc", "hdp2-n1.localdomain");
+    System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
+    System.setProperty("java.security.auth.login.config", "/Users/kyle/jaas.conf");
+    LoginContext context =
+        new LoginContext("kyle", new KerberosClientCallbackHandler("kyle",
+            "novirus"));
+    context.login();
 
-    ugi.doAs(new PrivilegedExceptionAction<Void>() {
+    Configuration conf = new Configuration();
+    conf.set("fs.defaultFS", "hdfs://10.1.192.76:8020");
 
-      public Void run() throws Exception {
-        String uri = "/tmp/seq/file1";
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://10.1.192.76:8020");
-        conf.set("hadoop.job.ugi", "kyle");
-        FileSystem fs = FileSystem.get(URI.create(uri), conf);
-        Path path = new Path(uri);
-        IntWritable key = new IntWritable();
-        Text value = new Text();
-        SequenceFile.Writer writer = null;
-        try {
-          writer =
-              SequenceFile.createWriter(fs, conf, path, key.getClass(),
-                  value.getClass());
-          for (int i = 0; i < 100; i++) {
-            key.set(100 - i);
-            value.set(DATA[i % DATA.length]);
-            System.out.printf("[% s]\t% s\t% s\n", writer.getLength(), key,
-                value);
-            writer.append(key, value);
-          }
-        } finally {
-          IOUtils.closeStream(writer);
-        }
-
-        return null;
+    String uri = "/tmp/seq/file1";
+    FileSystem fs = FileSystem.get(URI.create(uri), conf);
+    Path path = new Path(uri);
+    IntWritable key = new IntWritable();
+    Text value = new Text();
+    SequenceFile.Writer writer = null;
+    try {
+      writer =
+          SequenceFile.createWriter(fs, conf, path, key.getClass(),
+              value.getClass());
+      for (int i = 0; i < 100; i++) {
+        key.set(100 - i);
+        value.set(DATA[i % DATA.length]);
+        System.out.printf("[% s]\t% s\t% s\n", writer.getLength(), key, value);
+        writer.append(key, value);
       }
-    });
+    } finally {
+      IOUtils.closeStream(writer);
+    }
+  }
+
+  private static class KerberosClientCallbackHandler implements CallbackHandler {
+    private String username;
+    private String password;
+
+    public KerberosClientCallbackHandler(String username, String password) {
+      this.username = username;
+      this.password = password;
+    }
+
+    public void handle(Callback[] callbacks) throws IOException,
+        UnsupportedCallbackException {
+      for (Callback callback : callbacks) {
+        if (callback instanceof NameCallback) {
+          NameCallback ncb = (NameCallback) callback;
+          ncb.setName(username);
+        } else if (callback instanceof PasswordCallback) {
+          PasswordCallback pwcb = (PasswordCallback) callback;
+          pwcb.setPassword(password.toCharArray());
+        } else {
+          throw new UnsupportedCallbackException(callback, "We got a "
+              + callback.getClass().getCanonicalName()
+              + ", but only NameCallback and PasswordCallback is supported");
+        }
+      }
+    }
   }
 }
