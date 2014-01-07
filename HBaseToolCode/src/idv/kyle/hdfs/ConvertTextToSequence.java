42d51f194502bb77b0cef9b8f5ea999520b84b37
kylelin2000 <kylelin2000@fbfa5a9a-6df7-3eb1-ade7-53795e5f5e70>
2014-01-07 08:22:27 +0000

    [Hadoop Tool]save sequence file

---
+++ HBaseToolCode/src/idv/kyle/hdfs/ConvertTextToSequence.java
@@ -0,0 +1,56 @@
+package idv.kyle.hdfs;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+public class ConvertTextToSequence extends
+    Mapper<LongWritable, Text, LongWritable, Text> {
+
+  @Override
+  protected void map(LongWritable key, Text value,
+      Mapper<LongWritable, Text, LongWritable, Text>.Context context)
+      throws IOException, InterruptedException {
+
+    context.write(key, value);
+  }
+
+  public static void main(String[] args) throws IOException,
+      InterruptedException, ClassNotFoundException {
+    if (args.length != 2) {
+      System.out.println("Examples: executablejarfile /tmp/install.log /tmp/install.log.seq");
+      return;
+    }
+    Configuration conf = new Configuration();
+    Job job = new Job(conf);
+    job.setJobName("Convert Text");
+    job.setJarByClass(ConvertTextToSequence.class);
+
+    job.setMapperClass(Mapper.class);
+    job.setReducerClass(Reducer.class);
+
+    job.setNumReduceTasks(0);
+
+    job.setOutputKeyClass(LongWritable.class);
+    job.setOutputValueClass(Text.class);
+
+    job.setOutputFormatClass(SequenceFileOutputFormat.class);
+    job.setInputFormatClass(TextInputFormat.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    // submit and wait for completion
+    job.waitForCompletion(true);
+  }
+}
