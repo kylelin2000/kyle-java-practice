98dfe22ae48b036fd0b65e815b9f9c2251044e18
kylelin2000 <kylelin2000@fbfa5a9a-6df7-3eb1-ade7-53795e5f5e70>
2014-05-07 09:29:44 +0000

    [SolrTest] change packages.

--- SolrTest/src/idv/kyle/practice/solr/SolrCloudTest.java
+++ SolrTest/src/idv/kyle/practice/solr/SolrCloudTest.java
@@ -1,5 +1,7 @@
+package idv.kyle.practice.solr;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Iterator;
 
 import org.apache.solr.client.solrj.SolrQuery;
@@ -13,11 +15,18 @@ import org.apache.solr.common.cloud.ZkStateReader;
 
 public class SolrCloudTest {
 	public static void main(String[] args) throws Exception {
+		SolrCloudTest instance = new SolrCloudTest();
+		Date startDt = new Date();
+		instance.addIndex();
+		Date endDt = new Date();
+		System.out.println("Costed mili seconds: " + (endDt.getTime() - startDt.getTime()));
+	}
+	public  void addIndex() throws Exception {
 		//String url = "10.201.193.93:9983,10.201.193.94:9983,10.201.193.95:9983";
 		String url = "192.168.56.101:9983,192.168.56.101:9993,192.168.56.101:10003";
 		CloudSolrServer cloudSolrServer = new CloudSolrServer(url);
 		//cloudSolrServer.setDefaultCollection("luwakcollection");
-		cloudSolrServer.setDefaultCollection("c8");
+		cloudSolrServer.setDefaultCollection("c26");
 		cloudSolrServer.setZkClientTimeout(20000);
 		cloudSolrServer.setZkConnectTimeout(1000);
 		cloudSolrServer.connect();
@@ -27,30 +36,14 @@ public class SolrCloudTest {
 
 		System.out.println("Start Input...");
 		Collection<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
-		for (int i = 1; i < 4; i++) {
+		for (int i = 32001; i <= 62000; i++) {
 			SolrInputDocument doc = new SolrInputDocument();
-			doc.addField("id", "pencil " + i, 1.0f);
-			doc.addField("title", "cup " + i, 1.0f);
-			doc.addField("comments", "paper " + i, 1.0f);
-			doc.addField("cat", "light " + i, 1.0f);
+			doc.addField("id", "newid-" + i, 1.0f);
+			doc.addField("name", "name-" + i, 1.0f);
 			docs.add(doc);
 		}
 		cloudSolrServer.add(docs);
 		cloudSolrServer.commit();
-		
-		System.out.println("Start Query...");
-		SolrQuery query = new SolrQuery();
-		query.setQuery("*:*");
-		query.setStart(0);
-		query.setRows(100);
-		QueryResponse rsp = cloudSolrServer.query(query);
-		SolrDocumentList docsList = rsp.getResults();
-		System.out.println("Num Found: " + docsList.size());
-		for (Iterator<SolrDocument> solrDoc = docsList.iterator(); solrDoc.hasNext();) {
-			SolrDocument d = solrDoc.next();
-			System.out.print(d.getFieldValue("id") + "->");
-			System.out.println(d.getFieldValue("title"));
-		}
 	}
 
 }
