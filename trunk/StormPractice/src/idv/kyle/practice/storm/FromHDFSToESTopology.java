4158688199b73f0b2ee85a803776f6446c7d8da7
kylelin2000 <kylelin2000@fbfa5a9a-6df7-3eb1-ade7-53795e5f5e70>
2014-12-24 11:14:07 +0000

    add initial files for new project "StormPractice"

---
+++ trunk/StormPractice/src/idv/kyle/practice/storm/FromHDFSToESTopology.java
@@ -0,0 +1,155 @@
+package idv.kyle.practice.storm;
+
+import backtype.storm.Config;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.ShellBolt;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.elasticsearch.storm.EsBolt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FromHDFSToESTopology {
+
+  public static class HDFSFileReaderSpout extends BaseRichSpout {
+    static final Logger logger = LoggerFactory
+        .getLogger(HDFSFileReaderSpout.class);
+
+    SpoutOutputCollector _collector;
+    String _uri;
+
+    public HDFSFileReaderSpout(String uri) {
+      _uri = uri;
+    }
+
+    @Override
+    public void open(Map conf, TopologyContext context,
+        SpoutOutputCollector collector) {
+      _collector = collector;
+    }
+
+    @Override
+    public void nextTuple() {
+      InputStream in = null;
+      try {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(_uri), conf);
+        logger.info("fs: " + fs);
+        in = fs.open(new Path(_uri));
+        // IOUtils.copyBytes(in, System.out, 4096, false);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line = null;
+        while (null != (line = br.readLine())) {
+          _collector.emit(new Values(line));
+          Utils.sleep(100);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      } finally {
+        IOUtils.closeStream(in);
+      }
+    }
+
+    @Override
+    public void ack(Object id) {
+    }
+
+    @Override
+    public void fail(Object id) {
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+      declarer.declare(new Fields("word"));
+    }
+  }
+
+  public static class SplitSentence extends ShellBolt implements IRichBolt {
+    static final Logger logger = LoggerFactory.getLogger(SplitSentence.class);
+
+    public SplitSentence() {
+      super("python", "splitsentence.py");
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+      declarer.declare(new Fields("word"));
+    }
+
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+      return null;
+    }
+  }
+
+  public static class WordCount extends BaseBasicBolt {
+    static final Logger logger = LoggerFactory.getLogger(WordCount.class);
+
+    Map<String, Integer> counts = new HashMap<String, Integer>();
+
+    @Override
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
+      String word = tuple.getString(0);
+      Integer count = counts.get(word);
+      if (count == null)
+        count = 0;
+      count++;
+      counts.put(word, count);
+      collector.emit(new Values(word, count));
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+      declarer.declare(new Fields("word", "count"));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args == null || args.length != 3) {
+      System.out
+          .println("Wrong number of arguments!!!"
+              + "arg1: topology_name(Ex:TP), arg2: HDFS file path(Ex:hdfs://192.168.1.10:8020/tmp/maillog), arg3: ES index/type(Ex:stormtest/docs)");
+    } else {
+      TopologyBuilder builder = new TopologyBuilder();
+
+      builder.setSpout("spout", new HDFSFileReaderSpout(args[1]), 5);
+
+      builder.setBolt("split", new SplitSentence(), 8).shuffleGrouping("spout");
+      builder.setBolt("count", new WordCount(), 12).fieldsGrouping("split",
+          new Fields("word"));
+      builder.setBolt("es-bolt", new EsBolt(args[2]), 5).shuffleGrouping(
+          "count");
+
+      Config conf = new Config();
+      conf.setDebug(true);
+      conf.put("es.index.auto.create", "true");
+
+      conf.setNumWorkers(3);
+
+      StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+    }
+  }
+}
