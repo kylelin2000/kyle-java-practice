5eb4f8c90583fbb3d5dd3308274dac0da87e7d05
kylelin2000 <kylelin2000@fbfa5a9a-6df7-3eb1-ade7-53795e5f5e70>
2014-12-29 08:43:52 +0000

    update StormPractice

--- trunk/StormPractice/src/idv/kyle/practice/storm/ESWriteTopology.java
+++ trunk/StormPractice/src/idv/kyle/practice/storm/ESWriteTopology.java
@@ -3,12 +3,19 @@ package idv.kyle.practice.storm;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
+import backtype.storm.metric.LoggingMetricsConsumer;
+import backtype.storm.metric.api.CountMetric;
+import backtype.storm.metric.api.MeanReducer;
+import backtype.storm.metric.api.MultiCountMetric;
+import backtype.storm.metric.api.ReducedMetric;
 import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
@@ -87,18 +94,48 @@ public class ESWriteTopology {
     }
   }
 
-  public static class WordCount extends BaseBasicBolt {
+  public static class WordCount extends BaseRichBolt {
     Map<String, Integer> counts = new HashMap<String, Integer>();
+    OutputCollector _collector;
+
+    transient CountMetric _countMetric;
+    transient MultiCountMetric _wordCountMetric;
+    transient ReducedMetric _wordLengthMeanMetric;
 
     @Override
-    public void execute(Tuple tuple, BasicOutputCollector collector) {
+    public void prepare(Map conf, TopologyContext context,
+        OutputCollector collector) {
+      _collector = collector;
+
+      initMetrics(context);
+    }
+
+    void initMetrics(TopologyContext context) {
+      _countMetric = new CountMetric();
+      _wordCountMetric = new MultiCountMetric();
+      _wordLengthMeanMetric = new ReducedMetric(new MeanReducer());
+
+      context.registerMetric("execute_count", _countMetric, 5);
+      context.registerMetric("word_count", _wordCountMetric, 60);
+      context.registerMetric("word_length", _wordLengthMeanMetric, 60);
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
       String word = tuple.getString(0);
       Integer count = counts.get(word);
       if (count == null)
         count = 0;
       count++;
       counts.put(word, count);
-      collector.emit(new Values(word, count));
+      _collector.emit(new Values(word, count));
+      updateMetrics(word);
+    }
+
+    void updateMetrics(String word) {
+      _countMetric.incr();
+      _wordCountMetric.scope(word).incr();
+      _wordLengthMeanMetric.update(word.length());
     }
 
     @Override
@@ -122,6 +159,7 @@ public class ESWriteTopology {
     Config conf = new Config();
     conf.setDebug(true);
     conf.put("es.index.auto.create", "true");
+    conf.registerMetricsConsumer(LoggingMetricsConsumer.class, 2);
 
     if (args != null && args.length > 0) {
       conf.setNumWorkers(3);
