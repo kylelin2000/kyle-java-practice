1c440bd4a596a681f4ed2dbbec3bc4d947ed905f
kylelin2000 <kylelin2000@gmail.com>
2015-05-15 18:27:16 +0800

    fix spark streaming serialization issue

--- SparkPractice/src/idv/kyle/practice/spark/streaming/FromKafkaTest.java
+++ SparkPractice/src/idv/kyle/practice/spark/streaming/FromKafkaTest.java
@@ -38,17 +38,15 @@ public class FromKafkaTest {
     FileSystem fs = FileSystem.get(new Configuration());
     prop.load(new InputStreamReader(fs.open(pt)));
 
-    String zkHosts = prop.getProperty("zookeeper.host");
-    String kafkaGroup = prop.getProperty("kafka.group");
-    String kafkaTopics = prop.getProperty("kafka.topics");
-    String threadNumber = prop.getProperty("spark.kafka.thread.num");
+    final String zkHosts = prop.getProperty("zookeeper.host");
+    final String kafkaGroup = prop.getProperty("kafka.group");
+    final String kafkaTopics = prop.getProperty("kafka.topics");
+    final String threadNumber = prop.getProperty("spark.kafka.thread.num");
     final String esIndex = prop.getProperty("es.index");
     final String esNodes = prop.getProperty("es.nodes");
-    String walEnabled = prop.getProperty("spark.WAL.enabled");
-    String checkpointEnabled = prop.getProperty("spark.checkpoint.enabled");
     final String checkpointDirectory =
         prop.getProperty("spark.checkpoint.directory");
-    String reqAcks = prop.getProperty("kafka.request.required.acks");
+    final String reqAcks = prop.getProperty("kafka.request.required.acks");
     final int batchDuration =
         Integer.parseInt(prop.getProperty("spark.stream.batch.duration.ms"));
     
@@ -56,67 +54,51 @@ public class FromKafkaTest {
 
     JavaStreamingContext jssc;
 
-    if ("true".equals(walEnabled)) {
-      LOG.info("write WAL log");
+    JavaStreamingContextFactory contextFactory =
+        new JavaStreamingContextFactory() {
+          @Override
+          public JavaStreamingContext create() {
+            SparkConf sparkConf =
+                new SparkConf().setAppName("FromKafkaToES-Sync12");
+            sparkConf.set("es.index.auto.create", "true");
+            sparkConf.set("es.nodes", esNodes);
+            sparkConf.set("spark.streaming.receiver.writeAheadLog.enable",
+                "true");
+            LOG.info("es.index.auto.create set to "
+                + sparkConf.get("es.index.auto.create") + ", batchDuration is "
+                + batchDuration);
 
-      JavaStreamingContextFactory contextFactory =
-          new JavaStreamingContextFactory() {
-            @Override
-            public JavaStreamingContext create() {
-              SparkConf sparkConf =
-                  new SparkConf().setAppName("FromKafkaToES-Sync12");
-              sparkConf.set("es.index.auto.create", "true");
-              sparkConf.set("es.nodes", esNodes);
-              sparkConf.set("spark.streaming.receiver.writeAheadLog.enable",
-                  "true");
-              LOG.info("es.index.auto.create set to "
-                  + sparkConf.get("es.index.auto.create")
-                  + ", batchDuration is " + batchDuration);
+            JavaStreamingContext jssc =
+                new JavaStreamingContext(sparkConf, new Duration(batchDuration));
+            jssc.checkpoint(checkpointDirectory);
 
-              JavaStreamingContext jssc =
-                  new JavaStreamingContext(sparkConf, new Duration(
-                      batchDuration));
-              jssc.checkpoint(checkpointDirectory);
-              return jssc;
+            int numThreads = Integer.parseInt(threadNumber);
+            Map<String, Integer> topicMap = new HashMap<String, Integer>();
+            String[] topics = kafkaTopics.split(",");
+            for (String topic : topics) {
+              topicMap.put(topic, numThreads);
             }
-          };
 
-      jssc =
-          JavaStreamingContext.getOrCreate(checkpointDirectory, contextFactory);
-    } else {
-      SparkConf sparkConf = new SparkConf().setAppName("FromKafkaToES-Sync");
-      sparkConf.set("es.index.auto.create", "true");
-      sparkConf.set("es.nodes", esNodes);
-      LOG.info("es.index.auto.create set to "
-          + sparkConf.get("es.index.auto.create") + ", batchDuration is "
-          + batchDuration);
-      jssc = new JavaStreamingContext(sparkConf, new Duration(batchDuration));
-    }
+            Map<String, String> kafkaParams = new HashMap<String, String>();
+            kafkaParams.put("zookeeper.connect", zkHosts);
+            kafkaParams.put("group.id", kafkaGroup);
+            kafkaParams.put("serializer.class",
+                "kafka.serializer.StringEncoder");
+            kafkaParams.put("request.required.acks", reqAcks);
 
-    if ("true".equals(checkpointEnabled) || "true".equals(walEnabled)) {
-      LOG.info("enable checkpoint to directory " + checkpointDirectory);
-      jssc.checkpoint(checkpointDirectory);
-    }
+            JavaPairReceiverInputDStream<String, String> messages =
+                KafkaUtils.createStream(jssc, String.class, String.class,
+                    StringDecoder.class, StringDecoder.class, kafkaParams,
+                    topicMap, StorageLevel.MEMORY_AND_DISK_SER_2());
 
-    int numThreads = Integer.parseInt(threadNumber);
-    Map<String, Integer> topicMap = new HashMap<String, Integer>();
-    String[] topics = kafkaTopics.split(",");
-    for (String topic : topics) {
-      topicMap.put(topic, numThreads);
-    }
+            messages.print();
 
-    Map<String, String> kafkaParams = new HashMap<String, String>();
-    kafkaParams.put("zookeeper.connect", zkHosts);
-    kafkaParams.put("group.id", kafkaGroup);
-    kafkaParams.put("serializer.class", "kafka.serializer.StringEncoder");
-    kafkaParams.put("request.required.acks", reqAcks);
+            return jssc;
+          }
+        };
 
-    JavaPairReceiverInputDStream<String, String> messages =
-        KafkaUtils.createStream(jssc, String.class, String.class,
-            StringDecoder.class, StringDecoder.class, kafkaParams, topicMap,
-            StorageLevel.MEMORY_AND_DISK_SER_2());
-
-    messages.print();
+    jssc =
+        JavaStreamingContext.getOrCreate(checkpointDirectory, contextFactory);
 
     jssc.start();
     jssc.awaitTermination();
